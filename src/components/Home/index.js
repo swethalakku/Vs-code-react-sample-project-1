a631c0483c6f0523c5abdaddaca78b678d30aedf
lakku swetha <lakkuswetha111@gmail.com>
2023-10-12 15:42:19 +0530

    Add index.js to Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const Home = () => (
+    <div className="app-container">
+        <h1 className="heading">Hello, React</h1>
+        <p className="paragraph">I'm started to do the React.js Projects</p>
+        <button className="button">Get Started</button>
+    </div>
+)
+export default Home
